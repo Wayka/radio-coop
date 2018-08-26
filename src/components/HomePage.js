8ea612ffedf4213dc9aca08ec03b4cca050709c9
Eugenia.Soria <eugenia.soria@sovos.com>
2018-08-26 19:13:24 -0300

    Add logo

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   return (
     <div>
-      <h1>React Slingshot</h1>
-
+      <img src="imgs/logo.png"></img>
       <h2>Get Started</h2>
       <ol>
         <li>Review the <Link to="/fuel-savings">demo app</Link></li>
         <li>Remove the demo and start coding: npm run remove-demo</li>
       </ol>
+
     </div>
   );
 };
